fe9a5ad7211cdd10aece5387ebf9f12846fcf228
Pratik Dhoke <pratikdhoke13@gmail.com>
2020-09-16 01:25:51 +0530

    page iterations
    page iterations

--- pdf2audio.py
+++ pdf2audio.py
@@ -1,2 +1,21 @@
+#imports
 import pyttsx3
 import PyPDF2
+
+#pdf read
+book = open('oop.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+
+#number of pages
+pages = pdfReader.numPages
+
+#initialize speaker
+speaker = pyttsx3.init()
+
+#iterate pages
+for num in range(pages):
+    page = pdfReader.getPage(num)
+    print("page no:",+page)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
